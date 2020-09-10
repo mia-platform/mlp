ed1b5e80c8d9a2336ac85017b42fa0917cd9c192
Luca Terracciano <luca.terracciano@mia-platform.eu>
2020-09-10 16:55:13 +0200

    refactor: encapsulate script in cobra command

---
+++ cmd/interpolate.go
@@ -0,0 +1,155 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var primary_prefix string
+var alternative_prefix string
+var file_path string
+
+// interpolateCmd represents the interpolate command
+var interpolateCmd = &cobra.Command{
+	Use: "interpolate",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("no file specified")
+		}
+		_, err := os.Stat(args[0])
+		if os.IsNotExist(err) {
+			return errors.New("file " + args[0] + " does not exists")
+		}
+
+		return nil
+	},
+	Short: "Interpolate variables in file",
+	Long:  `Interpolate variables in file`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		file_path = args[0]
+		fmt.Println(file_path)
+		interpolate()
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(interpolateCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// interpolateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// interpolateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	interpolateCmd.Flags().StringVarP(&primary_prefix, "prefix", "p", "", "primary prefix to add when looking for envs")
+	interpolateCmd.Flags().StringVarP(&alternative_prefix, "alternative-prefix", "a", "", "prefix to use when the primary prefix env does not exists")
+}
+
+type env_var struct {
+	name  string
+	value string
+}
+
+func interpolate() {
+
+	file, err := ioutil.ReadFile(file_path)
+	check(err)
+
+	envs := getVariablesToInterpolate(file)
+
+	//exit if there are no variables to interpolate
+	if len(envs) == 0 {
+		os.Exit(0)
+	}
+
+	err = checkEnvs(envs)
+	check(err)
+
+	interpolated_file := interpolateVariables(file, envs)
+
+	f, err := os.Create("out-" + file_path)
+	check(err)
+	defer f.Close()
+
+	_, err = f.Write(interpolated_file)
+	check(err)
+
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getVariablesToInterpolate(file_content []byte) map[string]*env_var {
+	re := regexp.MustCompile("\\{\\{(.+?)\\}\\}")
+	match := re.FindAllStringSubmatch(string(file_content), -1)
+
+	vars := make(map[string]*env_var)
+	for parsed_var := range match {
+		var_name := strings.ReplaceAll(match[parsed_var][1], " ", "")
+		//keep track of the entire pattern found by the regex
+		//using as key the variable name
+		if _, exists := vars[var_name]; !exists {
+			vars[var_name] = &env_var{name: match[parsed_var][0]}
+		}
+	}
+	return vars
+}
+
+func checkEnvs(envs map[string]*env_var) error {
+	for var_name, _ := range envs {
+
+		var_prefixed := primary_prefix + "_" + var_name
+		var_prefixed_alternative := alternative_prefix + "_" + var_name
+		env := envs[var_name]
+
+		if os.Getenv(var_prefixed) != "" {
+			(*env).value = os.Getenv(var_prefixed)
+		} else if os.Getenv(var_prefixed_alternative) != "" {
+			(*env).value = os.Getenv(var_prefixed_alternative)
+		} else {
+			return errors.New("environment variables " + var_name + " does not exist")
+		}
+	}
+
+	return nil
+}
+
+func interpolateVariables(file []byte, envs map[string]*env_var) []byte {
+	file_string := string(file)
+
+	for var_name, _ := range envs {
+		env := *envs[var_name]
+		file_string = strings.ReplaceAll(file_string, env.name, env.value)
+	}
+
+	return []byte(file_string)
+}
--- main.go
+++ main.go
@@ -1,100 +1,22 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"errors"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"regexp"
-	"strings"
-)
-
-var primary_prefix = flag.String("prefix", "", "primary prefix appended to pick env vars")
-var fallback_prefix = flag.String("fallback-prefix", "", "prefix used when variables with the primary prefix does not existas")
-var file_path = flag.String("file", "", "file to interpolate")
+import "miadeploy/cmd"
 
 func main() {
-
-	flag.Parse()
-
-	if *primary_prefix == "" || *fallback_prefix == "" || *file_path == "" {
-		fmt.Println("Usage: -prefix PREFIX -fallback-prefix FALLBACK_PREFIX file")
-		os.Exit(1)
-	}
-
-	file, err := ioutil.ReadFile(*file_path)
-	check(err)
-
-	vars := getVariablesToInterpolate(file)
-
-	//exit if there are no variables to interpolate
-	if len(vars) == 0 {
-		os.Exit(0)
-	}
-
-	envs, err := checkEnvs(vars)
-	check(err)
-
-	interpolateVariables(file, vars, envs)
-}
-
-func check(err error) {
-	if err != nil {
-		fmt.Println("Error occurred: ", err)
-		os.Exit(1)
-	}
-}
-
-func getVariablesToInterpolate(file_content []byte) map[string]string {
-	re := regexp.MustCompile("\\{\\{(.+?)\\}\\}")
-	match := re.FindAllStringSubmatch(string(file_content), -1)
-
-	vars := make(map[string]string, 0)
-	for parsed_var := range match {
-		//keep track of the entire pattern found by the regex
-		//using as key the variable name
-		vars[match[parsed_var][1]] = match[parsed_var][0]
-	}
-	return vars
-}
-
-func checkEnvs(vars map[string]string) (map[string]string, error) {
-	envs := make(map[string]string, 0)
-	for var_name := range vars {
-
-		var_prefixed := *primary_prefix + "_" + var_name
-		var_prefixed_fallback := *fallback_prefix + "_" + var_name
-
-		fmt.Println(envs)
-
-		if os.Getenv(var_prefixed) != "" {
-			envs[var_name] = os.Getenv(var_prefixed)
-		} else if os.Getenv(var_prefixed_fallback) != "" {
-			envs[var_name] = os.Getenv(var_prefixed_fallback)
-		} else {
-			return nil, errors.New("environment variables " + var_name + " does not exist")
-		}
-	}
-
-	return envs, nil
-}
-
-func interpolateVariables(file []byte, vars map[string]string, envs map[string]string) bool {
-	file_string := string(file)
-
-	for var_name, _ := range envs {
-		file_string = strings.ReplaceAll(file_string, vars[var_name], envs[var_name])
-	}
-
-	fmt.Println(file_string)
-	f, err := os.Create("out-" + *file_path)
-	check(err)
-
-	written_bytes, err := f.WriteString(file_string)
-	fmt.Println("Successfully written ", written_bytes, " bytes")
-	check(err)
-	err = f.Close()
-	check(err)
-	return true
+	cmd.Execute()
 }
