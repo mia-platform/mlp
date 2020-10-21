b69827c2d17f43d85a4cb19bf3f97ada3dfdf335
Luca Terracciano <luca.terracciano@mia-platform.eu>
2020-10-21 17:21:34 +0200

    feat: add custom label when deploy resources

--- pkg/resourceutil/resource_test.go
+++ pkg/resourceutil/resource_test.go
@@ -6,6 +6,9 @@ import (
 	"testing"
 
 	"git.tools.mia-platform.eu/platform/devops/deploy/internal/utils"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/resource"
@@ -77,4 +80,100 @@ func TestMakeInfo(t *testing.T) {
 		require.Nil(t, err)
 		require.Equal(t, "default", info.Namespace, "Multiple objects in single yaml file currently not supported")
 	})
+}
+
+func TestMergeLabels(t *testing.T) {
+
+	testcases := []struct {
+		description string
+		message     string
+		expected    map[string]string
+		current     map[string]string
+		changes     map[string]string
+	}{
+		{
+			description: "Update value in map",
+			message:     "The value should be updated with the one contained in changes map",
+			expected: map[string]string{
+				"foo": "foo",
+				"bar": "bar",
+			},
+
+			current: map[string]string{
+				"foo": "foo",
+				"bar": "foo",
+			},
+
+			changes: map[string]string{
+				"bar": "bar",
+			},
+		},
+		{
+			description: "Add new key value in map",
+			message:     "The new key value should be present in the new map",
+			expected: map[string]string{
+				"foo":    "foo",
+				"bar":    "bar",
+				"foobar": "foo",
+			},
+
+			current: map[string]string{
+				"foo": "foo",
+				"bar": "bar",
+			},
+
+			changes: map[string]string{
+				"foobar": "foo",
+			},
+		},
+	}
+
+	for _, tt := range testcases {
+		t.Run(tt.description, func(t *testing.T) {
+			actual := mergeLabels(tt.current, tt.changes)
+			require.Equal(t, tt.expected, actual, tt.message)
+		})
+	}
+}
+
+func TestUpdateLabels(t *testing.T) {
+
+	testcases := []struct {
+		description string
+		message     string
+		expected    map[string]string
+		current     runtime.Object
+		changes     map[string]string
+	}{
+		{
+			description: "Add label to an object",
+			message:     "The updated object labels should contain the new key value",
+			expected: map[string]string{
+				"foo": "foo",
+				"bar": "bar",
+			},
+
+			current: &appsv1.Deployment{
+				TypeMeta: metav1.TypeMeta{APIVersion: appsv1.SchemeGroupVersion.String(), Kind: "Deployment"},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "bar",
+				},
+			},
+
+			changes: map[string]string{
+				managedByLabel: managedByMia,
+			},
+		},
+	}
+
+	for _, tt := range testcases {
+		t.Run(tt.description, func(t *testing.T) {
+			err := updateLabels(tt.current, tt.changes)
+			require.Nil(t, err)
+			labels, err := accessor.Labels(tt.current)
+			require.Nil(t, err)
+			require.Equal(t, labels[managedByLabel], managedByMia, tt.message)
+		})
+	}
 }
