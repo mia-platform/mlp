6581ed18e105c7c1a4f78550b3dd2b42a65e584f
Mattia Fumo <mattia.fumo@mia-platform.eu>
2022-05-13 14:31:46 +0200

    tests

--- pkg/resourceutil/sort_test.go
+++ pkg/resourceutil/sort_test.go
@@ -15,6 +15,7 @@
 package resourceutil
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -22,22 +23,33 @@ import (
 )
 
 func TestSortResourcesByKind(t *testing.T) {
-	resources := []Resource{
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "Unknown"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "Secret"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "ConfigMap"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "ClusterRole"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "IngressRoute"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "ClusterRoleBinding"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "ConfigMap"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "Deployment"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "PodSecurityPolicy"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "ServiceAccount"}},
-		{GroupVersionKind: &schema.GroupVersionKind{Kind: "Service"}},
-	}
-
 	t.Run("Reordering resources based on default reordering function", func(t *testing.T) {
-		expected := []string{"PodSecurityPolicy", "ServiceAccount", "ClusterRole", "ClusterRoleBinding", "Secret", "ConfigMap", "ConfigMap", "Service", "Deployment", "IngressRoute", "Unknown"}
+		resources := []Resource{
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "Unknown"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "Secret"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "ConfigMap"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "ClusterRole"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "IngressRoute"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "ClusterRoleBinding"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "ConfigMap"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "Deployment"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "PodSecurityPolicy"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "ServiceAccount"}},
+			{GroupVersionKind: &schema.GroupVersionKind{Kind: "Service"}},
+		}
+		expected := []string{
+			"PodSecurityPolicy",
+			"ServiceAccount",
+			"ClusterRole",
+			"ClusterRoleBinding",
+			"Secret",
+			"ConfigMap",
+			"ConfigMap",
+			"Service",
+			"Deployment",
+			"IngressRoute",
+			"Unknown",
+		}
 		var orderedNames []string
 		for _, resource := range SortResourcesByKind(resources, nil) {
 			orderedNames = append(orderedNames, resource.GroupVersionKind.Kind)
@@ -47,4 +59,61 @@ func TestSortResourcesByKind(t *testing.T) {
 			t.Fail()
 		}
 	})
+
+	t.Run("Reordering resources with mia-platform.eu/apply-before-kinds annotation", func(t *testing.T) {
+		resources := []Resource{
+			makeResourceWithAnnotation("UnknownBeforeNamespace", "Namespace"),
+			makeResourceWithAnnotation("UnknownBeforeEmpty", ""),
+			makeResourceWithAnnotation("UnknownBeforeDeploymentAndJob", "Job, Deployment"),
+			makeResourceWithAnnotation("UnknownBeforeDeploymentAndUnknown", "Deployment, Unknown"),
+			{
+				GroupVersionKind: &schema.GroupVersionKind{Kind: "Job"},
+			},
+			{
+				GroupVersionKind: &schema.GroupVersionKind{Kind: "Pod"},
+			},
+			{
+				GroupVersionKind: &schema.GroupVersionKind{Kind: "Deployment"},
+			},
+			{
+				GroupVersionKind: &schema.GroupVersionKind{Kind: "Namespace"},
+			},
+			{
+				GroupVersionKind: &schema.GroupVersionKind{Kind: "Unknown"},
+			},
+		}
+
+		expected := []string{
+			"UnknownBeforeNamespace",
+			"Namespace",
+			"Pod",
+			"UnknownBeforeDeploymentAndJob",
+			"UnknownBeforeDeploymentAndUnknown",
+			"Deployment",
+			"Job",
+			"UnknownBeforeEmpty",
+			"Unknown",
+		}
+		var orderedNames []string
+		for _, resource := range SortResourcesByKind(resources, nil) {
+			orderedNames = append(orderedNames, resource.GroupVersionKind.Kind)
+		}
+
+		if !reflect.DeepEqual(orderedNames, expected) {
+			fmt.Println(t.Name(), "reflect.DeepEqual failed")
+			fmt.Println("  expected:", expected)
+			fmt.Println("  actual:  ", orderedNames)
+			t.Fail()
+		}
+	})
+}
+
+func makeResourceWithAnnotation(kind string, applyBefore string) Resource {
+	resource := Resource{
+		GroupVersionKind: &schema.GroupVersionKind{Kind: kind},
+	}
+	resource.Object.SetAnnotations(map[string]string{
+		"mia-platform.eu/apply-before-kinds": applyBefore,
+	})
+	return resource
 }
