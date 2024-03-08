5d55c3c6046f65ffa7e49ed36f1cc8a16db7d900
Mattia Fumo <mattia.fumo@mia-platform.eu>
2024-03-08 16:28:20 +0100

    ignore gocyclo

--- pkg/deploy/apply.go
+++ pkg/deploy/apply.go
@@ -93,6 +93,8 @@ func withDeletableResource(apply applyFunction) applyFunction {
 // withAwaitableResource is an apply function decorator that awaits resources
 // decorated with awaitCompletionAnnotation for completion after they are
 // applied on the cluster
+//
+//gocyclo:ignore
 func withAwaitableResource(apply applyFunction) applyFunction {
 	return func(clients *k8sClients, res resourceutil.Resource, deployConfig utils.DeployConfig) error {
 		gvr, err := resourceutil.FromGVKtoGVR(clients.discovery, res.Object.GroupVersionKind())
