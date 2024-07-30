82d7dde5db58909b6e391233d9d4ef69999da4de
Jacopo Andrea Giola <jacopo@giola.org>
2024-07-30 10:34:54 +0200

    feat: add mixed deployment types e2e test

--- tests/e2e/integration_test.go
+++ tests/e2e/integration_test.go
@@ -15,7 +15,6 @@
 
 //go:build conformance
 
-//nolint:thelper
 package e2e
 
 import (
@@ -23,13 +22,16 @@ import (
 	"context"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/mia-platform/mlp/v2/pkg/cmd/deploy"
+	"github.com/mia-platform/mlp/v2/pkg/extensions"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
@@ -38,6 +40,7 @@ import (
 func TestDeployOnEmptyCluster(t *testing.T) {
 	deploymentFeature := features.New("deployment on empty cluster").
 		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			t.Helper()
 			t.Logf("starting test with kubeconfig %q and namespace %q", cfg.KubeconfigFile(), cfg.Namespace())
 
 			buffer := new(bytes.Buffer)
@@ -60,6 +63,8 @@ func TestDeployOnEmptyCluster(t *testing.T) {
 			return ctx
 		}).
 		Assess("resoures are being created", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			t.Helper()
+
 			deployment := new(appsv1.Deployment)
 			require.NoError(t, cfg.Client().Resources().Get(ctx, "test", cfg.Namespace(), deployment))
 			t.Logf("deployment found: %s", deployment.Name)
@@ -115,35 +120,79 @@ func TestDeployOnEmptyCluster(t *testing.T) {
 }
 
 func TestSmartDeploy(t *testing.T) {
-	deploymentFeature := features.New("smart deploy").
+	deploying := func(ctx context.Context, cfg *envconf.Config, deployType, stage string) {
+		buffer := new(bytes.Buffer)
+		deployCmd := deploy.NewCommand(genericclioptions.NewConfigFlags(false))
+		deployCmd.SetErr(buffer)
+		deployCmd.SetOut(buffer)
+
+		deployCmd.SetArgs([]string{
+			"--deploy-type",
+			deployType,
+			"--kubeconfig",
+			cfg.KubeconfigFile(),
+			"--namespace",
+			cfg.Namespace(),
+			"--filename",
+			filepath.Join("testdata", "smart-deploy", stage),
+		})
+
+		assert.NoError(t, deployCmd.ExecuteContext(ctx))
+		t.Log(buffer.String())
+		buffer.Reset()
+	}
+
+	var deployChecksum, dependenciesChecksum string
+
+	smartDeployPhase1 := features.New("deploy phase 1").
 		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			t.Helper()
 			t.Logf("starting test with kubeconfig %q and namespace %q", cfg.KubeconfigFile(), cfg.Namespace())
-
-			buffer := new(bytes.Buffer)
-			deployCmd := deploy.NewCommand(genericclioptions.NewConfigFlags(false))
-			deployCmd.SetErr(buffer)
-			deployCmd.SetOut(buffer)
-
-			deployCmd.SetArgs([]string{
-				"--deploy-type",
-				"smart_deploy",
-				"--kubeconfig",
-				cfg.KubeconfigFile(),
-				"--namespace",
-				cfg.Namespace(),
-				"--filename",
-				filepath.Join("testdata", "smart-deploy", "stage1"),
-			})
-
-			assert.NoError(t, deployCmd.ExecuteContext(ctx))
-			t.Log(buffer.String())
-			buffer.Reset()
+			deploying(ctx, cfg, extensions.DeployAll, "stage1")
 			return ctx
 		}).
-		Assess("smart deploy", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+		Assess("deploy phase 1", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			t.Helper()
+
+			deployment := new(appsv1.Deployment)
+			require.NoError(t, cfg.Client().Resources().Get(ctx, "test", cfg.Namespace(), deployment))
+			t.Logf("deployment found: %s", deployment.Name)
+			deployChecksum = deployment.Spec.Template.Annotations["mia-platform.eu/deploy-checksum"]
+			assert.NotEmpty(t, deployChecksum)
+			dependenciesChecksum = deployment.Spec.Template.Annotations["mia-platform.eu/dependencies-checksum"]
+			assert.NotEmpty(t, dependenciesChecksum)
+
+			secret := new(corev1.Secret)
+			require.NoError(t, cfg.Client().Resources().Get(ctx, "test", cfg.Namespace(), secret))
+			t.Logf("secret found: %s", secret.Name)
+
 			return ctx
 		}).
 		Feature()
 
-	testenv.Test(t, deploymentFeature)
+	smartDeployPhase2 := features.New("smart deploy phase 2").
+		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			t.Helper()
+			t.Logf("starting test with kubeconfig %q and namespace %q", cfg.KubeconfigFile(), cfg.Namespace())
+			deploying(ctx, cfg, extensions.DeploySmart, "stage2")
+			time.Sleep(2 * time.Second) // sleep to wait background deletions
+			return ctx
+		}).
+		Assess("smart deploy phase 2", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			t.Helper()
+			deployment := new(appsv1.Deployment)
+			require.NoError(t, cfg.Client().Resources().Get(ctx, "test", cfg.Namespace(), deployment))
+			t.Logf("deployment found: %s", deployment.Name)
+			assert.Equal(t, deployChecksum, deployment.Spec.Template.Annotations["mia-platform.eu/deploy-checksum"])
+			assert.NotEqual(t, dependenciesChecksum, deployment.Spec.Template.Annotations["mia-platform.eu/dependencies-checksum"])
+			assert.EqualValues(t, 2, deployment.Generation)
+
+			secret := new(corev1.Secret)
+			assert.True(t, apierrors.IsNotFound(cfg.Client().Resources().Get(ctx, "test", cfg.Namespace(), secret)))
+
+			return ctx
+		}).
+		Feature()
+
+	testenv.Test(t, smartDeployPhase1, smartDeployPhase2)
 }
